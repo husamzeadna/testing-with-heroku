96d7f4feca6bee99541b73682f667ab1127748cd
husamzeadna <abuelezzeyad43@gmail.com>
2022-02-01 14:50:01 +0200

    testing

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 require('./db/mongoose')
 const app = express();
+const publicPath = path.join(__dirname,'client/build');
 const port = process.env.PORT || 8000;
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
+app.use(publicPath);
 app.get('/api/users',(req,res)=>{
     try{
         res.status(200).send({username:'Bob',age:28})
